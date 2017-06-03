fd0ac8da1adbbb6e9d0d5fb3ca8abad217f431c1
Gerlando Savio Scibetta <scibetta.savio@gmail.com>
2017-06-03 17:05:47 +0200

    fixed variable scope (critical!)

--- neural_graph_machine.py
+++ neural_graph_machine.py
@@ -8,7 +8,7 @@ len_input = 1014
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-tf.flags.DEFINE_integer("evaluate_every", 10, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("evaluate_every", 2, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 1000, "Save model after this many steps (default: 100)")
 
 FLAGS = tf.flags.FLAGS
@@ -27,10 +27,10 @@ def g(input_x,num_classes=2, filter_sizes=(7, 7, 3), frame_size=32, num_hidden_u
     a = tf.expand_dims(a, 3)
 
     # Convolutional Layer 1
-    with tf.name_scope("conv-maxpool-1"):
+    with tf.variable_scope("conv-maxpool-1"):
         filter_shape = [num_quantized_chars, filter_sizes[0], 1, frame_size]
-        W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[frame_size]), name="b")
+        W = tf.get_variable("W",shape=filter_shape,initializer=tf.random_normal_initializer(stddev=0.05))
+        b = tf.get_variable("b",shape=[frame_size],initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(a, W, strides=[1, 1, 1, 1], padding="VALID", name="conv1")
         h = tf.nn.relu(tf.nn.bias_add(conv,b),name="relu")
         pooled = tf.nn.max_pool(
@@ -41,10 +41,10 @@ def g(input_x,num_classes=2, filter_sizes=(7, 7, 3), frame_size=32, num_hidden_u
             name="pool1")
 
     # Convolutional Layer 2
-    with tf.name_scope("conv-maxpool-2"):
+    with tf.variable_scope("conv-maxpool-2"):
         filter_shape = [1, filter_sizes[1], frame_size, frame_size]
-        W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[frame_size]), name="b")
+        W = tf.get_variable("W", shape=filter_shape, initializer=tf.random_normal_initializer(stddev=0.05))
+        b = tf.get_variable("b", shape=[frame_size], initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(pooled, W, strides=[1, 1, 1, 1], padding="VALID", name="conv2")
         h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
         pooled = tf.nn.max_pool(
@@ -55,10 +55,10 @@ def g(input_x,num_classes=2, filter_sizes=(7, 7, 3), frame_size=32, num_hidden_u
             name="pool2")
 
     # Convolutional Layer 3
-    with tf.name_scope("conv-maxpool-3"):
+    with tf.variable_scope("conv-maxpool-3"):
         filter_shape = [1, filter_sizes[2], frame_size, frame_size]
-        W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.05), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[frame_size]), name="b")
+        W = tf.get_variable("W", shape=filter_shape, initializer=tf.random_normal_initializer(stddev=0.05))
+        b = tf.get_variable("b", shape=[frame_size], initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(pooled, W, strides=[1, 1, 1, 1], padding="VALID", name="conv3")
         h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
         pooled = tf.nn.max_pool(
@@ -75,24 +75,24 @@ def g(input_x,num_classes=2, filter_sizes=(7, 7, 3), frame_size=32, num_hidden_u
     with tf.name_scope("dropout-1"):
         drop1 = tf.nn.dropout(h_pool_flat, dropout_keep_prob)
 
-    with tf.name_scope("fc-1"):
-        W = tf.Variable(tf.truncated_normal([num_features_total, num_hidden_units], stddev=0.05), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[num_hidden_units]), name="b")
+    with tf.variable_scope("fc-1"):
+        W = tf.get_variable("W", shape=[num_features_total, num_hidden_units], initializer=tf.random_normal_initializer(stddev=0.05))
+        b = tf.get_variable("b", shape=[num_hidden_units], initializer=tf.constant_initializer(0.1))
         fc_1_output = tf.nn.relu(tf.nn.xw_plus_b(drop1, W, b), name="fc-1-out")
 
     # Fully-connected Layer 2
     with tf.name_scope("dropout-2"):
         drop2 = tf.nn.dropout(fc_1_output, dropout_keep_prob)
 
-    with tf.name_scope("fc-2"):
-        W = tf.Variable(tf.truncated_normal([num_hidden_units, num_hidden_units], stddev=0.05), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[num_hidden_units]), name="b")
+    with tf.variable_scope("fc-2"):
+        W = tf.get_variable("W", shape=[num_hidden_units, num_hidden_units], initializer=tf.random_normal_initializer(stddev=0.05))
+        b = tf.get_variable("b", shape=[num_hidden_units], initializer=tf.constant_initializer(0.1))
         fc_2_output = tf.nn.relu(tf.nn.xw_plus_b(drop2, W, b), name="fc-2-out")
 
     # Fully-connected Layer 3
-    with tf.name_scope("output"):
-        W = tf.Variable(tf.truncated_normal([num_hidden_units, num_classes], stddev=0.05), name="W")
-        b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+    with tf.variable_scope("output"):
+        W = tf.get_variable("W", shape=[num_hidden_units, num_classes], initializer=tf.random_normal_initializer(stddev=0.05))
+        b = tf.get_variable("b", shape=[num_classes], initializer=tf.constant_initializer(0.1))
         scores = tf.nn.xw_plus_b(fc_2_output, W, b, name="output")
 
     return scores
@@ -124,17 +124,25 @@ def train_neural_network():
             cu1 = tf.placeholder(tf.float32, [None, ], name="CuLL")
             cv1 = tf.placeholder(tf.float32, [None, ], name="CvLL")
             cu2 = tf.placeholder(tf.float32, [None, ], name="CuLU")
+            test_input = tf.placeholder(tf.int32, [None, len_input, ], name="test_input")
+            test_labels = tf.placeholder(tf.float32, [None, 2], name="test_labels")
 
-            scores_u1 = g(in_u1)
-            scores_v1 = g(in_v1)
-            scores_u2 = g(in_u2)
+            with tf.variable_scope("ngm") as scope:
+                scores_u1 = g(in_u1)
+                scope.reuse_variables()
+                scores_v1 = g(in_v1)
+                scores_u2 = g(in_u2)
+                scores_v2 = g(in_v2)
+                scores_u3 = g(in_u3)
+                scores_v3 = g(in_v3)
+                test_scores = g(test_input, dropout_keep_prob=1.0)
 
             loss_function = tf.reduce_sum(alpha1 * weights_ll * tf.nn.softmax_cross_entropy_with_logits(logits=scores_u1, labels=tf.nn.softmax(scores_v1)) \
                             + cu1 * tf.nn.softmax_cross_entropy_with_logits(logits=scores_u1, labels=labels_u1) \
                             + cv1 * tf.nn.softmax_cross_entropy_with_logits(logits=scores_v1, labels=labels_v1)) \
-                            + tf.reduce_sum(alpha2 * weights_lu * tf.nn.softmax_cross_entropy_with_logits(logits=scores_u2, labels=tf.nn.softmax(g(in_v2))) \
+                            + tf.reduce_sum(alpha2 * weights_lu * tf.nn.softmax_cross_entropy_with_logits(logits=scores_u2, labels=tf.nn.softmax(scores_v2)) \
                             + cu2 * tf.nn.softmax_cross_entropy_with_logits(logits=scores_u2, labels=labels_u2)) \
-                            + tf.reduce_sum(alpha3 * weights_uu * tf.nn.softmax_cross_entropy_with_logits(logits=g(in_u3), labels=tf.nn.softmax(g(in_v3))))
+                            + tf.reduce_sum(alpha3 * weights_uu * tf.nn.softmax_cross_entropy_with_logits(logits=scores_u3, labels=tf.nn.softmax(scores_v3)))
 
             optimizer = tf.train.AdamOptimizer().minimize(loss_function, global_step=global_step)
 
@@ -143,17 +151,16 @@ def train_neural_network():
                                              tf.equal(tf.argmax(scores_u2, 1), tf.argmax(labels_u2, 1))],axis=0)
             train_accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
 
-            test_input = tf.placeholder(tf.int32, [None, len_input, ], name="test_input")
-            test_labels = tf.placeholder(tf.float32, [None, 2], name="test_labels")
+            saver = tf.train.Saver()
 
-            test_cp = tf.equal(tf.argmax(g(test_input, dropout_keep_prob=1.0), 1), tf.argmax(test_labels, 1))
+            test_cp = tf.equal(tf.argmax(test_scores, 1), tf.argmax(test_labels, 1))
             test_accuracy = tf.reduce_mean(tf.cast(test_cp, "float"), name="test_accuracy")
 
-            saver = tf.train.Saver()
-            writer = tf.summary.FileWriter('./summary')
-            writer.add_graph(sess.graph)
-            #sess.run(tf.global_variables_initializer())
-            saver.restore(sess, "./model2/model.ckpt")
+            sess.run(tf.global_variables_initializer())
+
+            variables_names = [v.name for v in tf.trainable_variables()]
+            print(variables_names)
+
             num_epochs = 10
             for epoch in range(num_epochs):
                 print("======== EPOCH " + str(epoch + 1) + " ========")
@@ -161,7 +168,7 @@ def train_neural_network():
                 batches = batch_iter(batch_size=128)
                 accs = list()
                 test_accs = list()
-                test_batches = test_batch_inter(256)
+                test_batches = test_batch_inter(128)
 
                 for batch in batches:
                     current_step = tf.train.global_step(sess, global_step)
@@ -200,5 +207,4 @@ def train_neural_network():
 
 
                     if current_step % FLAGS.checkpoint_every == 0:
-                        save_path = saver.save(sess, "./model.ckpt")
-            #save_path = saver.save(sess, "./model.ckpt")
+                        saver.save(sess, "./model.ckpt")
